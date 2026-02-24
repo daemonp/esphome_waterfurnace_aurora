014ee03a132bf55ad1bb0952a557de0b9b5d69a3
damon <d@disassemble.net>
2026-02-24 11:15:26 -0500

    fix: remove double component registration in water_heater
    new_water_heater() internally calls register_water_heater() which
    already calls cg.register_component(). The explicit second call caused:
    ValueError: Component ID aurora_dhw was not declared to inherit from
    Component, or was registered twice.
    
    Also remove redundant .extend(cv.COMPONENT_SCHEMA) since
    water_heater_schema() already includes it via _WATER_HEATER_SCHEMA.

--- components/waterfurnace_aurora/water_heater/__init__.py
+++ components/waterfurnace_aurora/water_heater/__init__.py
@@ -11,20 +11,17 @@ AuroraWaterHeater = waterfurnace_aurora_ns.class_(
     "AuroraWaterHeater", water_heater.WaterHeater, cg.Component
 )
 
-CONFIG_SCHEMA = (
-    water_heater.water_heater_schema(AuroraWaterHeater, icon="mdi:water-boiler")
-    .extend(
-        {
-            cv.GenerateID(CONF_AURORA_ID): cv.use_id(WaterFurnaceAurora),
-        }
-    )
-    .extend(cv.COMPONENT_SCHEMA)
+CONFIG_SCHEMA = water_heater.water_heater_schema(
+    AuroraWaterHeater, icon="mdi:water-boiler"
+).extend(
+    {
+        cv.GenerateID(CONF_AURORA_ID): cv.use_id(WaterFurnaceAurora),
+    }
 )
 
 
 async def to_code(config):
     var = await water_heater.new_water_heater(config)
-    await cg.register_component(var, config)
 
     parent = await cg.get_variable(config[CONF_AURORA_ID])
     cg.add(var.set_parent(parent))
